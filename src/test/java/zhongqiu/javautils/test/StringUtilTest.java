0eeb8fbc5d82be13f74a0ac4a9404664044ca9c0
leon66666 <1902518544@qq.com>
2017-06-05 16:07:06 +0800

    测试代码

---
+++ src/test/java/zhongqiu/javautils/test/StringUtilTest.java
@@ -0,0 +1,12 @@
+package zhongqiu.javautils.test;
+
+import zhongqiu.javautils.StringUtil;
+
+/**
+ * Created by wangzhongqiu on 2017/6/5.
+ */
+public class StringUtilTest {
+    public static void main(String[] args) {
+        System.out.print(StringUtil.chineseLength("王中"));
+    }
+}
